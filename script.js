312065bbcf1ea5ac5260f64e80679cf6f779f427
Tanha <Tanha005akter@gmail.com>
2024-06-02 00:52:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const mins = document.getElementById('mins');
+const secs = document.getElementById('secs');
+
+
+const formatTime = (time) => {
+    return time < 10 ? `0$(time)` : time;
+ }
+
+const updateCountDown = (deadline) => {
+    const currentTime = new Date();
+    const timeDifference = deadline - currentTime; 
+
+    // calclulate days, hours, mins, secs from timedifferent
+    let calSecs = Math.floor(timeDifference / 1000) % 60;
+    let calcMins = Math.floor(timeDifference / 1000/60) % 60;
+    let calcHours = Math.floor(timeDifference / 1000/60/60) % 24;
+    let calcDays = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
+   
+
+days.textContent = formatTime(calcDays);
+mins.textContent = formatTime(calcMins);
+hours.textContent = formatTime(calcHours);
+secs.textContent = formatTime(calSecs);
+
+}
+
+const countDown = (targetDate) => {
+setInterval(() => updateCountDown(targetDate), 1000);
+};
+
+const targetDate = new Date("June 21 2024 07:30");
+countDown(targetDate);
